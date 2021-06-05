6b58352e4bc039e543b6105fb55aae5a0c49d1a1
bekzhan-ai <scroll028@gmail.com>
2021-06-05 22:47:54 +0600

    2

---
+++ ObjectClone.java
@@ -0,0 +1,43 @@
+package Main;
+
+class ObjectClone  implements Cloneable{
+
+    ObjectClone () {
+    }
+
+    ObjectClone (boolean clone) {
+        this.clone = clone;
+    }
+
+    public void setClone(boolean clone) {
+        this.clone = clone;
+    }
+
+    boolean clone;
+
+
+
+    @Override
+    public ObjectClone  clone() throws CloneNotSupportedException {
+        return (ObjectClone) super.clone();
+    }
+
+
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        ObjectClone test = new ObjectClone (false);
+        ObjectClone clonedTest = test.clone();
+
+        System.out.println("клон: "+clonedTest.clone);
+        System.out.println("оригинал: "+test.clone);
+
+        System.out.println("--------------");
+
+        test.setClone(true);
+
+        System.out.println("клон: "+clonedTest.clone);
+        System.out.println("оригинал: "+test.clone);
+
+        //нужен для клонирования объектов
+    }
+}
