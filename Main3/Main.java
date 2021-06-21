48de146299edac38b32b50829a69b51f9b9c005f
bekzhan-ai <scroll028@gmail.com>
2021-06-21 22:09:01 +0600

    37.1

--- Main3/Main.java
+++ Main3/Main.java
@@ -1,43 +1,21 @@
 package Main3;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
 
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String>books = new ArrayList<>();
+        Stack<String> cars = new Stack<>();
+        cars.push("Audi");
+        cars.push("BMW");
+        cars.push("Chevrolet");
+        cars.push("Citroen");
+        cars.push("Hyundai");
 
-        books.add("Меняя маски");
-        books.add("Небесный шаг");
-        books.add("Дитя");
-        books.add("Хрономаг");
-        books.add("Резчик");
-        System.out.println(books);
-
-        String get = books.remove(books.size() - 1);
-        System.out.println(books);
-
-        books.set(books.size() - 1, get);
-        System.out.println(books.get(books.size() - 1));
-
-        books.clear();
-
-        books.add("Искатель ветра");
-        books.add("Нерождённый");
-        books.add("Вечный Данж");
-        System.out.println(books);
-
-        LinkedList<String> stringLinkedList = new LinkedList<>(books);
-        stringLinkedList.addFirst("Искатель");
-        stringLinkedList.addFirst("Искатель");
-
-        stringLinkedList.addLast("Пустошь. Нулевой круг");
-        stringLinkedList.addLast("Пустошь. Нулевой круг");
-
-        stringLinkedList.removeFirstOccurrence("Искатель");
-        stringLinkedList.removeLastOccurrence("Пустошь. Нулевой круг");
-
-        System.out.println(stringLinkedList);
+        System.out.println(cars);
+        cars.pop();
+        System.out.println(cars.peek());
+        cars.search("Audi");
     }
+
 }
